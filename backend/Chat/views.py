f2b5855fc7086cdf03c11032b2c44ea040a17aad
donjoo <donjorois@gmail.com>
2025-06-02 15:21:28 +0530

    permission clases

--- backend/Chat/views.py
+++ backend/Chat/views.py
@@ -1,8 +1,10 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from .models import Room, Message
 from users.models import CustomUser
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
+
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
